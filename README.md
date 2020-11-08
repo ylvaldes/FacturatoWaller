d1a33570945d44320d1125b1f7859f353400a433
Yasmani Ledesma <yledesma@ute.com.uy>
2020-11-08 17:51:00 -0300

    Update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FacturatoWaller  [![Build Status](https://travis-ci.org/ylvaldes/FacturatoWaller.svg?branch=master)](https://travis-ci.org/ylvaldes/FacturatoWaller)
-De las facturas de los mercados de UY te genera un excel para importar a Waller
+De las facturas de los mercados de UY te genera un excel para importar a Wallet
+
+https://web.budgetbakers.com/dashboard
+
--- src/main/java/com/ylvaldes/leerpdf/LeerPDF.java
+++ src/main/java/com/ylvaldes/leerpdf/LeerPDF.java
@@ -215,8 +215,9 @@ public class LeerPDF {
 					// Descarga el Fichero
 
 					Date d;
-					d = format2.parse(result.get(5));
+					d = format3.parse(result.get(5).replace("%2F", ""));
 					String fecha = format.format(d);
+					
 					num = Integer.parseInt(result.get(3));
 					if (!new File(recurso.getResourse() + FILE_SEPARATOR + result.get(1) + result.get(2) + num.toString() + ".pdf").exists()) {
 						ObtenerPDF obtenerPDF = new ScantechTest();
